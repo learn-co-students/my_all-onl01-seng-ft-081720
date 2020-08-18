3ff00ff46935d4213df3050ff88523d51d733d2b
Eli Cusic <tntitans31@gmail.com>
2020-08-18 18:19:37 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i = 0
+    freshy = []
+    while i < collection.length
+        freshy << yield(collection[i])
+        i += 1
+    end
+    freshy.include?(false) ? false : true
 end
